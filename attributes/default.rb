e8942319843d07bb902e8273e04ebef53c9e45cb
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-09-30 13:32:05 -0300

    Improve setting the attributes 'user' and 'app_group'

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,10 +6,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-default['mconf-home']['user'] = 'mconf'
-default['mconf-home']['app_group'] = 'www-data'
-
-default['mconf-home']['domain'] = '192.168.0.100'
-default['mconf-home']['deploy_to'] = '/var/www/mconf-home'
+default['mconf-home']['user']           = node['mconf']['user'] || 'mconf'
+default['mconf-home']['app_group']      = node['mconf']['app_group'] || 'www-data'
+default['mconf-home']['domain']         = '192.168.0.100'
+default['mconf-home']['deploy_to']      = '/var/www/mconf-home'
 default['mconf-home']['deploy_to_full'] = '/var/www/mconf-home/current'
-default['mconf-home']['remove_www'] = true
+default['mconf-home']['remove_www']     = true
